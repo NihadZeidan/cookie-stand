182da2ea8e9ee221a9179a2f0624daace7713d55
NihadZeidan <nihadzeidan@gmail.com>
2021-02-17 21:36:35 +0200

    first try lab09

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@
 
 function randomCus(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-};
+}
 
 
 let hours = ['6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM'];
 
+let city = [];
 
-function Country(locationName, minCustomers, maxCustomers, avgCookiesPerCus, randomCustomers, cookiesPerHour, total, totalPerHour, locations) {
+function Country(locationName, minCustomers, maxCustomers, avgCookiesPerCus) {
     this.locationName = locationName;
     this.minCustomers = minCustomers;
     this.maxCustomers = maxCustomers;
@@ -16,16 +17,14 @@ function Country(locationName, minCustomers, maxCustomers, avgCookiesPerCus, ran
     this.randomCustomers = 0;
     this.cookiesPerHour = [];
     this.total = 0;
-
-
+    city.push(this);
 }
-console.log(Country.locations);
 
 Country.prototype.perHour = function () {
     for (let j = 0; j < hours.length; j++) {
         this.randomCustomers = (randomCus(this.minCustomers, this.maxCustomers));
         this.cookiesPerHour[j] = Math.floor(this.avgCookiesPerCus * this.randomCustomers);
-        this.total += this.cookiesPerHour[j]
+        this.total += this.cookiesPerHour[j];
         this.cookiesPerHour.push(this.total);
 
     }
@@ -49,15 +48,14 @@ Country.prototype.createTabel = function () {
 
 
 
-let table = document.getElementById('parent')
-table.setAttribute("style", "text-align: center;")
+let table = document.getElementById('parent');
 
 let createHeadingRow = function () {
-    let firstRow = document.createElement('tr')
-    table.appendChild(firstRow)
-    let firstCell = document.createElement('th')
-    firstCell.textContent = " Time"
-    firstRow.appendChild(firstCell)
+    let firstRow = document.createElement('tr');
+    table.appendChild(firstRow);
+    let firstCell = document.createElement('th');
+    firstCell.textContent = " Time";
+    firstRow.appendChild(firstCell);
 
 
     for (let i = 0; i < hours.length; i++) {
@@ -72,56 +70,81 @@ let createHeadingRow = function () {
 
 }
 
+
 createHeadingRow();
 
-
-
+console.log(city);
 
 
 let seattle = new Country('Seattle ', 23, 65, 6.3);
-seattle.perHour();
-seattle.createTabel();
-console.log(seattle);
-
 let tokyo = new Country('Tokyo ', 3, 24, 1.2);
-tokyo.perHour();
-tokyo.createTabel();
-console.log(tokyo);
-
 let dubai = new Country('Dubai ', 11, 38, 3.7);
-dubai.perHour();
-dubai.createTabel();
-console.log(dubai);
-
 let paris = new Country('Paris ', 20, 38, 2.3);
-paris.perHour();
-paris.createTabel();
-console.log(paris);
-
 let lima = new Country('Lima ', 2, 16, 4.6);
-lima.perHour();
-lima.createTabel();
-console.log(lima);
+
+for (let i = 0; i < city.length; i++) {
+    city[i].perHour();
+    city[i].createTabel();
+}
 
 
 
-let createFinalRow = function () {
+let finalRow = document.createElement('tr')
+table.appendChild(finalRow)
 
-    let finalRow = document.createElement('tr')
-    table.appendChild(finalRow)
-    let data2 = document.createElement('th')
-    finalRow.appendChild(data2)
-    data2.textContent = "Total"
+
+function createFinalRow () {
+    let totalDataFirstCell = document.createElement('th')
+    finalRow.appendChild(totalDataFirstCell)
+    totalDataFirstCell.textContent = "Total"
 
     for (let i = 0; i <= hours.length; i++) {
-        let data = document.createElement('th')
-        finalRow.appendChild(data)
-        let totalOfTotal = seattle.cookiesPerHour[i] + tokyo.cookiesPerHour[i] + dubai.cookiesPerHour[i] + paris.cookiesPerHour[i] + lima.cookiesPerHour[i]
-        data.textContent = totalOfTotal
-        console.log(totalOfTotal)
-    }
+        let totalData = document.createElement('th');
+        finalRow.appendChild(totalData);
+        let totalOfEach = 0;
 
+        for (let y = 0; y < city.length; y++) {
+
+            totalOfEach += city[y].cookiesPerHour[i]
+            totalData.textContent = totalOfEach
+       }
+    }
 }
 
 createFinalRow();
 
+
+
+let createForm = document.getElementById('createCity');
+createForm.addEventListener('submit', addingLocation);
+
+
+function addingLocation (event) {
+    event.preventDefault();
+
+    let name = event.target.locationName.value;
+    console.log(name);
+
+    let min = event.target.minCustomer.value;
+    console.log(min);
+
+    let max = event.target.maxCustomer.value;
+    console.log(max);
+
+    let avg = event.target.avgCustomer.value;
+    console.log(avg);
+
+    let locationAddedByUser = new Country(name, min, max, avg);
+    locationAddedByUser.perHour();
+    locationAddedByUser.createTabel();
+
+    let toClearTheTable = document.getElementById('parent')
+    toClearTheTable.textContent = " ";
+    
+    for (let i = 0; i < city.length; i++) {
+        city[i].perHour();
+        city[i].createTabel();
+    }
+    
+    createFinalRow();
+}  
